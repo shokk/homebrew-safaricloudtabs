038b7d5345a52142c1a1879f66c6e2173321989c
Ernest Oporto <ernieoporto@gmail.com>
2026-06-09 17:34:07 -0400

    chore: bump to v1.0.1, fix homepage URL

--- Formula/safaricloudtabs.rb
+++ Formula/safaricloudtabs.rb
@@ -1,8 +1,8 @@
 class Safaricloudtabs < Formula
   desc "Inspect Safari iCloud tab groups from the command line"
-  homepage "https://github.com/shokk/safari-cloud-tabs"
-  url "https://github.com/shokk/homebrew-safaricloudtabs/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "7238437030bcb3f50f91039632669335e5908a1ee4bb9d577e6c5b98b4faca5d"
+  homepage "https://github.com/shokk/homebrew-safaricloudtabs"
+  url "https://github.com/shokk/homebrew-safaricloudtabs/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "92174438b757a73aecc8c40e10f9a647001d8588be922f0378fd81b2cef4096e"
   license "MIT"
 
   depends_on "jq"
