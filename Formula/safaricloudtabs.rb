eba0cf769ff221fe6dd5b0f96cd611e2a7ee10b7
Ernest Oporto <ernieoporto@gmail.com>
2026-06-09 17:10:21 -0400

    fix: point formula at homebrew-safaricloudtabs releases

--- Formula/safaricloudtabs.rb
+++ Formula/safaricloudtabs.rb
@@ -1,8 +1,8 @@
 class Safaricloudtabs < Formula
   desc "Inspect Safari iCloud tab groups from the command line"
   homepage "https://github.com/shokk/safari-cloud-tabs"
-  url "https://github.com/shokk/safari-cloud-tabs/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "a8e38235ccd451f08e02704ebd84528b20e2d60e98e831ee81c5115c6959de6e"
+  url "https://github.com/shokk/homebrew-safaricloudtabs/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "7238437030bcb3f50f91039632669335e5908a1ee4bb9d577e6c5b98b4faca5d"
   license "MIT"
 
   depends_on "jq"
