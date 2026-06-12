de577ce70d892ed5a2dfa1bca15c3281febab9ba
Ernest Oporto <ernieoporto@gmail.com>
2026-06-12 13:16:15 -0400

    chore: bump formula to v1.0.2

--- Formula/safaricloudtabs.rb
+++ Formula/safaricloudtabs.rb
@@ -1,8 +1,8 @@
 class Safaricloudtabs < Formula
   desc "Inspect Safari iCloud tab groups from the command line"
   homepage "https://github.com/shokk/homebrew-safaricloudtabs"
-  url "https://github.com/shokk/homebrew-safaricloudtabs/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "92174438b757a73aecc8c40e10f9a647001d8588be922f0378fd81b2cef4096e"
+  url "https://github.com/shokk/homebrew-safaricloudtabs/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "716406eb3e34c959de5dcdea27f447da8f4fcc083b3b266a7a1add352ca2870e"
   license "MIT"
 
   depends_on "jq"
